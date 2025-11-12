59e6621281500aa16f8e0568fd38983153cc4684
MichaelSandilands <michael.b.sandilands@gmail.com>
2025-11-12 21:10:49 +0000

    Added npm

--- .devcontainer/nvim/lua/kickstart/plugins/lspconfig.lua
+++ .devcontainer/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -211,15 +211,15 @@ return {
         -- clangd = {},
         -- gopls = {},
         pyright = {
-          settings = {
-            python = {
-              analysis = {
-                diagnosticSeverityOverrides = {
-                  reportUnusedExpression = 'none',
-                },
-              },
-            },
-          },
+          -- settings = {
+          --   python = {
+          --     analysis = {
+          --       diagnosticSeverityOverrides = {
+          --         reportUnusedExpression = 'none',
+          --       },
+          --     },
+          --   },
+          -- },
         },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
